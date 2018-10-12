86d6325f36bc20a6dbc4b1bf5377f8daa7f8fb0b
Adam Giegel <adam.giegel@gmail.com>
2018-10-12 15:28:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 def speak_to_grandma(string)
   if string == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
-  elsif string == string.downcase
-    return 'HUH?! SPEAK UP, SONNY!'
+    puts 'I LOVE YOU TOO PUMPKIN!'
+  elsif string != string.downcase
+    puts 'HUH?! SPEAK UP, SONNY!'
   else
-    return 'NO, NOT SINCE 1938!'
+    puts 'NO, NOT SINCE 1938!'
   end
 end
     
